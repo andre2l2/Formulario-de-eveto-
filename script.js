5c89a2495dd5947e670021ba05bc6376b84f7439
Andre <delcferreira@gmail.com>
2020-05-24 00:02:41 -0300

    v1.0

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const $menu = document.querySelector('.menu');
 const $btnMenu_open = document.querySelector('.btnMenu_open');
 const $btnMenu_close = document.querySelector('.btnMenu_close');
+const $time1 = document.querySelector('.time1');
+const $time2 = document.querySelector('.time2');
+const $userInputTime = document.querySelector('.userInput-time');
 
 $btnMenu_open.addEventListener('click', () => {
     $menu.classList.add('menu_open');
@@ -8,4 +11,13 @@ $btnMenu_open.addEventListener('click', () => {
 
 $btnMenu_close.addEventListener('click', () => {
     $menu.classList.remove('menu_open');
+})
+
+
+$time1.addEventListener('click', () => {
+    $userInputTime.classList.add('remove');
+})
+
+$time2.addEventListener('click', () => {
+    $userInputTime.classList.remove('remove');
 })
